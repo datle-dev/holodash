536484733446addcd3877fdc704bc1214904b80b
datle-dev <dle.dev@outlook.com>
2024-07-23 13:01:06 -0400

    Add TanStack Query devtools

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import {
   QueryClient,
   QueryClientProvider
 } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import App from './App.tsx'
 import './index.css'
 
@@ -13,6 +14,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
+      <ReactQueryDevtools initialIsOpen={true} />
     </QueryClientProvider>
   </React.StrictMode>,
 )
